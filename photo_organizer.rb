5a7c2188caee519fb3d4e640ce45642169e261d0
AileenBull <aileen.cacayorin@gmail.com>
2024-05-09 11:35:01 -0500

    more refactoring, code clean up

--- photo_organizer.rb
+++ photo_organizer.rb
@@ -1,31 +1,6 @@
 require 'irb'
 require 'date'
 
-test_str = "photo.jpg, Krakow, 2013-09-05 14:08:15
-Mike.png, London, 2015-06-20 15:13:22
-myFriends.png, Krakow, 2013-09-05 14:07:13
-Eiffel.jpg, Florianopolis, 2015-07-23 08:03:02
-pisatower.jpg, Florianopolis, 2015-07-22 23:59:59
-BOB.jpg, London, 2015-08-05 00:02:03
-notredame.png, Florianopolis, 2015-09-01 12:00:00
-me.jpg, Krakow, 2013-09-06 15:40:22
-a.png, Krakow, 2016-02-13 13:33:50
-b.jpg, Krakow, 2016-01-02 15:12:22
-c.jpg, Krakow, 2016-01-02 14:34:30
-d.jpg, Krakow, 2016-01-02 15:15:01
-e.png, Krakow, 2016-01-02 09:49:09
-f.png, Krakow, 2016-01-02 10:55:32
-g.jpg, Krakow, 2016-02-29 22:13:11"
-
-#TO DO:
-# split string into a hash with keys: name, ext?, city, datetime, index? --> DONE
-# group hash by city, then sort by datetime --> DONE
-# assign number determine new photo name --> DONE
-# return new string --> DONE
-# tests
-# refactor
-# readMe
-
 def parse_photos_from_string(string)
   string.split("\n").map.with_index {|photo, index| parse_photo_details(photo, index) }
 end
@@ -49,15 +24,12 @@ def group_and_sort_photos(photo_arr)
     sorted_photos[city] = photos.sort_by { |photo| photo[:datetime] }
   end
   sorted_photos
-
 end
 
 def determine_new_photo_names(photo_hash)
   photo_hash.each do |city, photos|
-    count = photos.length
-    len = count/10 + 1
     photos.map.with_index(1) do |photo, i|
-      photo[:new_name] = "#{city}#{i.to_s.rjust(len, '0')}"
+      photo[:new_name] = "#{city}#{i.to_s.rjust(photos.length/10 + 1, '0')}"
     end
   end
 end
@@ -70,11 +42,27 @@ def output_new_photo_str(photo_hash)
 end
 
 def solution(str)
-  photo_arr = parse_photos_from_string(str)
-  sorted_photos = group_and_sort_photos(photo_arr)
-  new_name_photos = determine_new_photo_names(sorted_photos)
+  photo_arr = parse_photos_from_string(str)                     # turns string into an a array of hashes that contain photo details
+  sorted_photos = group_and_sort_photos(photo_arr)              # groups photos by city and sorts each group by date
+  new_name_photos = determine_new_photo_names(sorted_photos)    # creates new photo name based on city and photo count per city
 
-  output_new_photo_str(sorted_photos)
+  output_new_photo_str(sorted_photos)                           # returns string of newly named photos in the original input order
 end
 
- puts solution(test_str)
+test_str = "photo.jpg, Krakow, 2013-09-05 14:08:15
+Mike.png, London, 2015-06-20 15:13:22
+myFriends.png, Krakow, 2013-09-05 14:07:13
+Eiffel.jpg, Florianopolis, 2015-07-23 08:03:02
+pisatower.jpg, Florianopolis, 2015-07-22 23:59:59
+BOB.jpg, London, 2015-08-05 00:02:03
+notredame.png, Florianopolis, 2015-09-01 12:00:00
+me.jpg, Krakow, 2013-09-06 15:40:22
+a.png, Krakow, 2016-02-13 13:33:50
+b.jpg, Krakow, 2016-01-02 15:12:22
+c.jpg, Krakow, 2016-01-02 14:34:30
+d.jpg, Krakow, 2016-01-02 15:15:01
+e.png, Krakow, 2016-01-02 09:49:09
+f.png, Krakow, 2016-01-02 10:55:32
+g.jpg, Krakow, 2016-02-29 22:13:11"
+
+puts solution(test_str)
